a6e1a478603fbb007cdeb9ae5a3dcd149323fca3
bdcorps <bdcorps@gmail.com>
2017-11-12 23:18:02 -0500

    add files

---
+++ src/index.js
@@ -0,0 +1,48 @@
+import _ for 'Lodash'
+import React,{Component} from 'react';
+import ReactDOM from 'react-dom';
+import YTSearch from 'youtube-api-search';
+
+import SearchBar from './components/search_bar';
+import VideoList from './components/video_list';
+import VideoDetail from './components/video_detail';
+
+const API_KEY = "AIzaSyC-gk3ZwN-lSs4S26-v4XxdkTocHyw3tG4"
+
+class App extends Component {
+	constructor(props){
+		super(props);
+
+		this.state = {videos:[],
+			selectedVideo: null};
+
+this.videoSearch('machine learning');
+	}
+
+	videoSearch(term){
+
+		YTSearch({key: API_KEY, term:term},  (videos)=>
+	{
+	this.setState ({videos:videos,
+		selectedVideo: videos[0]});
+	});
+	}
+
+	render (){
+const videoSearch = ._debounce((term) => {this.videoSearch(term)}, 300);
+
+		return (
+			<div>
+			<SearchBar onSearchTermChange = {videoSearch}/>
+			<VideoDetail video
+			={this.state.selectedVideo}/>
+			<VideoList 
+onVideoSelect={selectedVideo => this.setState({selectedVideo})}
+			videos={this.state.videos} />
+			</div>
+			);
+	}
+}
+
+
+ReactDOM.render (<App />, document.querySelector('.container'));
