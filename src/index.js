f62fcc2e511ff50742d8ec345be2253783e551d4
Minho Lee <minho.lee.93@hotmail.com>
2017-11-13 15:09:53 -0500

    testing out and formatting

--- src/index.js
+++ src/index.js
@@ -13,34 +13,35 @@ class App extends Component {
 	constructor(props){
 		super(props);
 
-		this.state = {videos:[],
+		this.state = {
+			videos:[],
 			selectedVideo: null};
 
-this.videoSearch('machine learning');
+		this.videoSearch('machine learning');
 	}
 
 	videoSearch(term){
-
 		YTSearch({key: API_KEY, term:term},  (videos)=>
-	{
-	this.setState ({videos:videos,
-		selectedVideo: videos[0]});
-	});
-	}
+		{
+			this.setState ({videos:videos,
+				selectedVideo: videos[0]});
+			});
+		}
 
 	render (){
-const videoSearch = _.debounce((term) => {this.videoSearch(term)}, 300);
-
+		const videoSearch = _.debounce((term) => {this.videoSearch(term)}, 300);
 		return (
 			<div>
-			<SearchBar onSearchTermChange = {videoSearch}/>
-			<VideoDetail video
-			={this.state.selectedVideo}/>
-			<VideoList 
-onVideoSelect={selectedVideo => this.setState({selectedVideo})}
-			videos={this.state.videos} />
+				<SearchBar onSearchTermChange = {videoSearch}/>
+				<VideoDetail 
+					video={this.state.selectedVideo}
+				/>
+				<VideoList 
+					onVideoSelect={selectedVideo => this.setState({selectedVideo})}
+					videos={this.state.videos} 
+				/>
 			</div>
-			);
+		);
 	}
 }
 
